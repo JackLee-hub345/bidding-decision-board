252a16e7573a05dda01a2402a19c028ae43c5056
JackLee-hub345 <jackleedunkaliyun@gmail.com>
2026-07-05 17:09:47 +0800

    Add files via upload

--- bidding_V6.1.py
+++ bidding_V6.1.py
@@ -18,7 +18,6 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 plt.rcParams['font.sans-serif'] = ['WenQuanYi Zen Hei', 'SimHei', 'Microsoft YaHei', 'Arial Unicode MS']
-plt.rcParams['font.sans-serif'] = ['SimHei', 'Microsoft YaHei', 'Arial Unicode MS'] 
 plt.rcParams['axes.unicode_minus'] = False 
 
 st.title("📊 投标报价智能决策看板 v6.2")
